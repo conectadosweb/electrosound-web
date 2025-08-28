aaf1efa3df8a248ac7b6619343568dbfe790dc0d
conectadosweb <web.conectados.arg@gmail.com>
2025-08-28 11:07:11 -0300

    Nuevo color

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ const ALLOWED_ORIGINS = new Set([
   'http://127.0.0.1:4000',
   'http://localhost',
   'https://www.electrosoundpack.com',
-  'https://www.electrosoundpack.com',
+  'https://www.electrosoundpack.com.ar',
   'https://electrosound-web.onrender.com'
 ]);
 app.use(cors({
